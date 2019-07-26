4e1473b4fe48813d8eb806cecffbd51dfdf9074c
Otis-TWDD <otis@twdd.com.tw>
2019-07-26 14:45:55 +0800

    cookie

--- crawler/crawler.go
+++ crawler/crawler.go
@@ -50,7 +50,7 @@ func getAllArticles() {
 	req, err := http.NewRequest("GET", url, nil)
 	req.Header.Add("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36")
 	req.Header.Add("Referer", baseUrl)
-	cookie := http.Cookie{
+	cookie := http.Cookie {
 		Name: "over18",
 		Value: "1"
 	}
