014c5eb335ffc65febc1036e139feae1b3d58d6f
Otis-TWDD <otis@twdd.com.tw>
2019-07-29 10:23:27 +0800

    adding logs

--- crawler/crawler.go
+++ crawler/crawler.go
@@ -31,22 +31,24 @@ func getAllArticles(fourm string) {
 
 	var BOOL = true
 	var exist = true
-	var url string = ""  //default url
-	var href string = "" //next page url
+	var url string = ""  // default url
+	var href string = "" // next page url
 	var crawlerCount = 0
 
-	//today's date
+	// today's date
 	loc, _ := time.LoadLocation("Asia/Chongqing")
 	time := time.Now().In(loc)
 
+	// 開始爬蟲
 	for BOOL {
+
 		if href == "" {
-			url = BasePttAddress + "/bbs/" + fourm + "/index.html"
+			url = BasePttAddress + "/bbs/" + fourm + "/index.html" // 首頁
 		} else {
-			url = BasePttAddress + href
+			url = BasePttAddress + href // 翻至下一頁
 		}
 
-
+	// 設定 header 以及 滿18歲cookie
 	client:=&http.Client{}
 	req, err := http.NewRequest("GET", url, nil)
 	req.Header.Add("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36")
@@ -58,8 +60,8 @@ func getAllArticles(fourm string) {
 	req.AddCookie(&cookie)
 	res, err := client.Do(req)
 	defer res.Body.Close()
-	//最后直接把res传给goquery就可以来解析网页了
 
+	// 最後直接把res傳给goquery就可以來解析網頁
 		doc, err := goquery.NewDocumentFromResponse(res)
 		if err != nil {
 			log.Fatal(err)
@@ -79,14 +81,16 @@ func getAllArticles(fourm string) {
 			hrefs, _ := s.Find(".title a").Attr("href")
 			article.Link = BasePttAddress + hrefs
 			article.Date = strings.TrimSpace(s.Find(".meta").Find(".date").Text())
-			article.ImageLink = "https://i.imgur.com/aQjMlmV.jpg"
+			article.ImageLink = "https://i.imgur.com/aQjMlmV.jpg" // 先塞入預設imageLink
 			article.LikeCountString = s.Find(".nrec span").Text()
 			if article.Date != time.Format("1/02") {
 				if crawlerCount > 0 {
-					BOOL = false
+					BOOL = false // 爬不到今日文章後 停止爬蟲
 				}
 			}
 
+			// 今日文章且未被刪除（被刪除文章url會變成BasePttAddress)
+			// 若文章內含有https及.jpg 的字串, 儲存為article.ImageLink.
 			if article.Date == time.Format("1/02") && article.Link != BasePttAddress {
 				//search image link in article
 				doc, err := goquery.NewDocument(article.Link)
@@ -94,7 +98,6 @@ func getAllArticles(fourm string) {
 					log.Fatal(err)
 				}
 
-				//article-metaline
 				doc.Find("#main-content > a").EachWithBreak(func(i int, s *goquery.Selection) bool {
 					imgLink := s.Text()
 					if strings.Contains(imgLink, ".jpg") {
@@ -105,9 +108,11 @@ func getAllArticles(fourm string) {
 					}
 					return true
 				})
+				log.Println("第" + i + "筆資料:" + article.Date + " 標題: " + article.Title)
 				db.InsertArticle(article.Title, article.LikeCount, article.Link, article.Date, article.ImageLink, article.LikeCountString)
 			}
 		})
 		crawlerCount = crawlerCount + 1
 	}
+
 }
