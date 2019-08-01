fc848bf7c5e08cf596e980d2e6f1e51d5488dfee
Otis-TWDD <otis@twdd.com.tw>
2019-08-01 11:02:51 +0800

    getAllBoard

--- crawler/crawler.go
+++ crawler/crawler.go
@@ -52,7 +52,7 @@ func getHotBoards() {
 		}
 
 		doc.Find(".b-ent a").Each(func(i int, s *goquery.Selection) {
-			boradName := s.Find("board").Attr("board-name")
+			boradName, _ := s.Find("board").Attr("board-name")
 			log.Print(boradName)
 		})
 
